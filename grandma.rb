a802c643bfa2c18498f62b916f589c60bfef1467
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-01-14 15:34:41 -0500

    commit tree 3fff4aefc03d61cace592a6b353f525ba86f134c

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,15 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+
+def speak_to_grandma(response)
+   if response == "I love you Grandma!".upcase 
+    return "I LOVE YOU TOO PUMPKIN!"
+   elsif response == response.upcase
+    return "NO, NOT SINCE 1938!"
+   elsif response != response.upcase 
+    return "HUH?! SPEAK UP, SONNY!"
+   end 
+
+end
